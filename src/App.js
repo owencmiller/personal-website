d0ce86cbcb4dcdd251793873d3a407385d949a88
Owen Miller <owenchristianmiller@gmail.com>
2020-10-03 01:36:52 -0400

    restructure and create navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Background}  from './components/Background';
 import Home from './Pages/Home';
 import Experiments from './Pages/Experiments';
+import NavBar from './components/NavBar'
 
 class App extends React.Component {
   render() {
     return(
-        <div>
-          <Home/>
-          <Experiments/>
-        </div>
+      <Router>
+        <Switch>
+          <Route path="/experiments">
+            <Experiments />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }

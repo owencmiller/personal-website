45bcb0af81be65b0624ff778ce15339aeba32973
Owen Miller <owenchristianmiller@gmail.com>
2021-08-06 21:52:40 -0400

    add blog navigation and new blog post

---
+++ src/Pages/Blog/BlogPosts/posts.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {BlogHeader, BlogImage, BlogText, BlogImageWrapper, BlogPost} from '../styles'
+
+// TODO - clean up image imports
+import img1 from '../../../../assets/img/acadia/IMG_0004.JPG'
+import img2 from '../../../../assets/img/acadia/IMG_0100.JPG'
+import img3 from '../../../../assets/img/acadia/IMG_0123.JPG'
+import img4 from '../../../../assets/img/acadia/IMG_2303.JPG'
+import img5 from '../../../../assets/img/acadia/IMG_0003.JPG'
+import img6 from '../../../../assets/img/acadia/IMG_0021.JPG'
+import img7 from '../../../../assets/img/acadia/IMG_0060.JPG'
+import img8 from '../../../../assets/img/acadia/IMG_0093.JPG'
+import img9 from '../../../../assets/img/acadia/IMG_0116.JPG'
+
+
+export const acadia = 
+<BlogPost>
+    <BlogHeader>
+    Acadia National Park
+    </BlogHeader>
+    <BlogText>
+    &emsp; &emsp; First stop is Acadia National Park in Maine! Great hiking, awesome views, and I'm learning a lot about living in a van. So 
+    far the hardest parts have been finding a place to park and dealing with smelly hiking shoes. Nonetheless, so far so good!
+    </BlogText>
+    <BlogImageWrapper>
+        <BlogImage src= {img4}/>
+        <BlogImage src= {img1}/>
+        <BlogImage src= {img2}/>
+        <BlogImage src= {img3}/>
+        <BlogImage src= {img5}/>
+        <BlogImage src= {img6}/>
+        <BlogImage src= {img7}/>
+        <BlogImage src= {img8}/>
+        <BlogImage style={{maxWidth: "70%"}} src= {img9}/>
+    </BlogImageWrapper>
+</BlogPost>
+
+import img10 from '../../../../assets/img/cuyahoga/IMG_0305.JPG'
+import img11 from '../../../../assets/img/cuyahoga/IMG_0324.JPG'
+import img12 from '../../../../assets/img/cuyahoga/IMG_0331.JPG'
+import img13 from '../../../../assets/img/cuyahoga/IMG_0336.JPG'
+import img14 from '../../../../assets/img/cuyahoga/IMG_0347.JPG'
+import img15 from '../../../../assets/img/cuyahoga/IMG_0361.JPG'
+import img16 from '../../../../assets/img/cuyahoga/IMG_0372.JPG'
+import img17 from '../../../../assets/img/cuyahoga/IMG_0383.JPG'
+import img18 from '../../../../assets/img/cuyahoga/IMG_0395.JPG'
+
+
+export const cuyahoga = 
+<BlogPost>
+    <BlogHeader>
+    Cuyahoga Valley National Park
+    </BlogHeader>
+    <BlogText>
+    &emsp; &emsp; Second stop was Cuyahoga Valley National Park in Ohio! More good hiking and some interesting history. There was a beautiful stream 
+    with only an couple of inches of water running through it which made the classic "babbling brook" sound. Two parks down and a lot more to go!
+    </BlogText>
+    <BlogImageWrapper>
+        <BlogImage src= {img10}/>
+        <BlogImage src= {img11}/>
+        <BlogImage src= {img12}/>
+        <BlogImage src= {img17}/>
+        <BlogImage src= {img15}/>
+        <BlogImage src= {img16}/>
+        <BlogImage src= {img18}/>
+        <BlogImage src= {img13}/>
+        <BlogImage style={{maxWidth: "70%"}} src= {img14}/>
+    </BlogImageWrapper>
+</BlogPost>
--- src/Pages/Blog/index.js
+++ src/Pages/Blog/index.js
@@ -1,41 +1,26 @@
-import React from 'react';
-import {BlogHeader, BlogImage, BlogText, BlogWrapper} from './styles'
+import React, {useState} from 'react';
 import NavBar from '../../components/NavBar'
 import {BasicPage} from '../BasicPage/styles'
+import {acadia, cuyahoga} from './BlogPosts/posts'
+import { BlogNav, BlogNavLink } from './styles';
 
-// TODO - clean up image imports
-import img1 from '../../../assets/img/IMG_0004.png'
-import img2 from '../../../assets/img/IMG_0100.png'
-import img3 from '../../../assets/img/IMG_0123.png'
-import img4 from '../../../assets/img/IMG_2304.png'
-import img5 from '../../../assets/img/IMG_0003.png'
-import img6 from '../../../assets/img/IMG_0021.png'
-import img7 from '../../../assets/img/IMG_0060.png'
-import img8 from '../../../assets/img/IMG_0093.png'
-import img9 from '../../../assets/img/IMG_0116.png'
+const posts = [acadia, cuyahoga]
 
 function Blog (props){
+    const [page, setPage] = useState(0)
+
     return (
         <BasicPage>
             <NavBar />
-            <BlogHeader>
-                Acadia National Park
-            </BlogHeader>
-            <BlogText>
-                &emsp; &emsp; First stop is Acadia National Park in Maine! Great hiking, awesome views, and I'm learning a lot about living in a van. So 
-                far the hardest parts have been finding a place to park and dealing with smelly hiking shoes. Nonetheless, so far so good!
-            </BlogText>
-            <BlogWrapper>
-                <BlogImage src= {img4}/>
-                <BlogImage src= {img1}/>
-                <BlogImage src= {img2}/>
-                <BlogImage src= {img3}/>
-                <BlogImage src= {img5}/>
-                <BlogImage src= {img6}/>
-                <BlogImage src= {img7}/>
-                <BlogImage src= {img8}/>
-                <BlogImage src= {img9}/>
-            </BlogWrapper>
+            <BlogNav>
+                <BlogNavLink onClick={() => setPage(0)}>
+                    Acadia
+                </BlogNavLink>
+                <BlogNavLink onClick={() => setPage(1)}>
+                    Cuyahoga
+                </BlogNavLink>
+            </BlogNav>
+            {posts[page]}
         </BasicPage>
     )
 }
